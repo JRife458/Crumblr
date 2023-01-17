d86a9b959377f05f8ec7678d2f52691832399e9f
Justin <mancussion@gmail.com>
2023-01-16 20:36:05 -0500

    added likes seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .post import seed_posts, undo_posts
+from .like import seed_likes, undo_likes
 
 from app.models.db import db, environment, SCHEMA
 
@@ -19,8 +20,10 @@ def seed():
         # Make sure to add all your other model's undo functions below
         undo_users()
         undo_posts()
+        undo_likes()
     seed_users()
     seed_posts()
+    seed_likes()
     # Add other seed functions here
 
 
@@ -29,4 +32,5 @@ def seed():
 def undo():
     undo_users()
     undo_posts()
+    undo_likes()
     # Add other undo functions here
